439993fdedbeda2a840fbb545a4d398a15499c8d
RyosukeIchinose <ryosuke_130116@outlook.jp>
2023-08-26 15:36:46 +0900

    header作成

--- Home/Home.js
+++ Home/Home.js
@@ -2,6 +2,11 @@ $(function(){
     let humbergerButton = $("#humberger-button");
     let headerNav = $("#header-nav");
     let navCloseButton = $("#nav-close-button");
+    let homeImgArray = $("#home-img-container img");
+    let section = $("#section");
+    let nextIndex = 0;
+    let currentIndex = 0;
+
 
     humbergerButton.on('click', function(){
         headerNav.fadeIn();
@@ -10,4 +15,17 @@ $(function(){
     navCloseButton.on('click', function(){
         headerNav.fadeOut();
     })
+
+    setInterval(function(){
+        nextIndex = (currentIndex + 1) % homeImgArray.length;
+        homeImgArray.eq(currentIndex).animate({
+            opacity: '0',            
+        }, 2000)
+        homeImgArray.eq(nextIndex).animate({
+            opacity: '1'
+        }, 2000)
+        homeImgArray.eq(nextIndex).css('opacity', 1);
+
+        currentIndex = nextIndex;
+    }, 7000)
 })
